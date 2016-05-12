dbd1565bc54cd2febfc370b506a1b9f93d5126b4
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2016-05-12 02:02:08 -0400

    Fix for the initializer

--- app/src/SecurityContextTokenInitializer.ts
+++ app/src/SecurityContextTokenInitializer.ts
@@ -1,11 +1,12 @@
 import {SecurityContext} from './SecurityContext';
+import {ISecurityContextInitializer} from './ISecurityContextInitializer';
 
 import {IdentityFactory} from './Identities/IdentityFactory';
 import {Identity} from './Identities/Identity';
 
 //export default SecurityContextTokenInitializer;
 
-export class SecurityContextTokenInitializer implements SecurityContextTokenInitializer
+export class SecurityContextTokenInitializer implements ISecurityContextInitializer
 {
     constructor(public tokens :any[]) 
     {
