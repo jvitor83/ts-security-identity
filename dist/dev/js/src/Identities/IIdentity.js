6427faddaf173561c0a47746be0413c2f51c5cd0
Joaquim Rodolfo Marçal Galli <rodolfocop@gmail.com>
2018-01-29 09:13:56 -0300

    Update packages to typings and add orchestrator for build in gulp dependencis

--- dist/dev/js/src/Identities/IIdentity.js
+++ dist/dev/js/src/Identities/IIdentity.js
@@ -1,5 +1,3 @@
-// import {IPermissaoObjeto} from './IPermissao';
-// export default IIdentity;
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
@@ -13,4 +11,4 @@
     Object.defineProperty(exports, "__esModule", { value: true });
 });
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9JZGVudGl0aWVzL0lJZGVudGl0eS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxpREFBaUQ7QUFDakQsNEJBQTRCIiwiZmlsZSI6InNyYy9JZGVudGl0aWVzL0lJZGVudGl0eS5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCB7SVBlcm1pc3Nhb09iamV0b30gZnJvbSAnLi9JUGVybWlzc2FvJztcclxuLy8gZXhwb3J0IGRlZmF1bHQgSUlkZW50aXR5O1xyXG5cclxuZXhwb3J0IGludGVyZmFjZSBJSWRlbnRpdHkge1xyXG4gIC8vIG5ldyhDbGFpbXM6IEFycmF5PHsga2V5OiBzdHJpbmcsIHZhbHVlOiBhbnkgfT4pO1xyXG4gIENsYWltczogQXJyYXk8e2tleTogc3RyaW5nLCB2YWx1ZTogYW55fT47XHJcblxyXG4gIE5hbWU6IHN0cmluZztcclxuXHJcbiAgSXNBdXRoZW50aWNhdGVkOiBib29sZWFuO1xyXG59XHJcbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvSWRlbnRpdGllcy9JSWRlbnRpdHkuanMiLCJzb3VyY2VzQ29udGVudCI6W119
